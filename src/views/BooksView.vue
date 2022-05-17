38f8d832c1bb4abcc9cde9e68b70b9b1b84177cf
jojogames632 <jojogames@free.fr>
2022-05-17 09:42:52 +0200

    add xl responsive size for sidenav

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="md:flex">
-		<div class="w-[100vw] md:w-[35vw] lg:w-[22vw]">
+		<div class="w-[100vw] md:w-[35vw] lg:w-[22vw] xl:w-[16vw]">
 			<SideNav />
 		</div>
 		<div class="md:w-[65vw] lg:w-[78vw]">
 			<input
 				type="text"
 				placeholder="Rechercher un livre..."
-				class="border border-gray-300 rounded"
+				class="border border-gray-300 rounded mt-6 text-center mx-auto"
 				/>
 		</div>
 	</div>
