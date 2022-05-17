802b57de393e11778bd6983c3161514cee87d77c
jojogames632 <jojogames@free.fr>
2022-05-17 10:05:52 +0200

    Add type filters in books page

--- src/views/BooksView.vue
+++ src/views/BooksView.vue
@@ -7,18 +7,49 @@
 			<input
 				type="text"
 				placeholder="Rechercher un livre..."
-				class="border border-gray-300 rounded mt-6 text-center mx-auto"
+				class="border border-gray-300 rounded mt-6 text-center"
+				v-model="search"
 				/>
+			
+			<div class="grid grid-cols-3 md:grid-cols-1 text-sm mt-6">
+				<div 
+					class="mb-4 p-2 rounded"
+					v-for="(type, index) in types" 
+					:class="{ 'bg-lime-200': currentType === type }"
+					:key="index" 
+					@click="selectType(type)">
+					{{ type }}
+				</div>
+			</div>
+
 		</div>
 	</div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 import SideNav from '../components/Shared/SideNav.vue'
 
 export default {
 	components: {
 		SideNav
+	},
+	setup() {
+		const search = ref('')
+		const types = ref([
+			'Développement personnel',
+			'Spiritualité',
+			'Alimentation',
+			'Finance',
+			'Productivité'
+		])
+		const currentType = ref('')
+
+		const selectType = (type) => {
+			currentType.value = type === currentType.value ? '' : type
+		}
+
+		return { search, types, currentType, selectType }
 	}
 }
 </script>
